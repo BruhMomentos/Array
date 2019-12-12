3933cf8ef0a0ceb9238c65874bf8253fd7162b2a
BruhMomentos <55457974+BruhMomentos@users.noreply.github.com>
2019-12-12 10:00:04 -0800

    dada

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//Create an array that holds 20 positions, give each index a
+//random value between 1-6, then use a for loop to count
+//how many 1's were randomly generated in  the array
+
+        int[] ints = new ints[20];
+
+
+
+
+    }
+}
